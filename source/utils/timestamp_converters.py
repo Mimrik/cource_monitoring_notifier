9c7b67c8918b86b06420c5c2950372cb7022da4d
Mimrik <misha@mamrov.ru>
2024-04-23 21:25:16 +0300

    pytz added

--- source/utils/timestamp_converters.py
+++ source/utils/timestamp_converters.py
@@ -1,4 +1,7 @@
 import time
+from datetime import datetime
+
+import pytz
 from sqlalchemy import text
 
 
@@ -7,3 +10,8 @@ CURRENT_TIMESTAMP_SEC_SQL_CLAUSE = text("EXTRACT(EPOCH FROM NOW())")
 
 def get_current_time_sec() -> int:
     return int(time.time())
+
+
+def localize_and_cast_date_title(timestamp: int, timezone_code: str) -> str:
+    timezone = pytz.timezone(timezone_code)
+    return datetime.fromtimestamp(timestamp).astimezone(timezone).strftime('%Y-%m-%d %H:%M:%S')
